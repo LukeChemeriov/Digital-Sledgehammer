d106addc2643882fa8bad54ffd2ee9d03f5260f9
Luke <dgm.bailey@outlook.com>
2022-05-26 15:49:58 +0300

    Altered build scripts to make sure it actually works lol

---
+++ build-ds.bat
@@ -0,0 +1,2 @@
+del bin/ds-start
+g++ -c -Wall -Wextra -Werror src/Main.cpp -o bin/ds-start
--- build/build-ds.bat
+++
@@ -1,3 +0,0 @@
-cd ../
-g++ -c -Wall -Wextra -Werror -src/Main.cpp -o build/intermediates/Main.o 
-g++ -o bin/ds-start.exe build/intermediates/Main.o
