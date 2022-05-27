c61ecdba90732114877f0340dbd9e411e40fa869
Luke <dgm.bailey@outlook.com>
2022-05-27 17:24:23 +0300

    Made some permission changes to ds-start file

--- build-ds.sh
+++ build-ds.sh
@@ -2,5 +2,6 @@
 
 rm bin/ds-start
 g++ -c -Wall -Wextra -Werror src/Main.cpp -o bin/ds-start
+sudo chmod +x bin/ds-start
 # To build for Windows, change the `ds-start` to `ds-start.exe`
 # For a UNIX OS (Linux, macOS), just leave it as it is
