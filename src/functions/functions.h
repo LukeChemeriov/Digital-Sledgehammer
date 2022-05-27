25dd7e618ee90b14e07b5315643a3dff014e5a6e
Luke <dgm.bailey@outlook.com>
2022-05-27 17:28:37 +0300

    Implemented the new DS_IO class

--- src/functions/functions.h
+++ src/functions/functions.h
@@ -8,9 +8,12 @@
                     // rather poke my eyes out with sharp sticks...
 #include "../lib/io.h"
 
+// Initialisation of IO class
+DS_IO io;
+
 // A simple echo function
 void echoFunction(const std::string & userinput)
 {
     // Let's print the text the user gave us
-    std::cout << userinput << std::endl;
+     io.kprintf(userinput);
 }
