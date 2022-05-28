63d5dbdfaa745f7ee7a207b410f986b0a812571d
Luke <dgm.bailey@outlook.com>
2022-05-28 13:35:15 +0300

    Removed uneccasary chmod

--- build-ds.sh
+++ build-ds.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 rm bin/ds-start
-g++ -c -Wall -Wextra -Werror src/Main.cpp -o bin/ds-start
-sudo chmod +x bin/ds-start
+g++ -c -Wall -Wextra -Werror src/Main.cpp -o bin/ds-star
 # To build for Windows, change the `ds-start` to `ds-start.exe`
 # For a UNIX OS (Linux, macOS), just leave it as it is
