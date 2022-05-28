08e1bec2dd536840d4210054aa6fe7a4c0819e49
Luke <dgm.bailey@outlook.com>
2022-05-28 13:42:33 +0300

    Fixed output error

--- build-ds.sh
+++ build-ds.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 rm bin/ds-start
-g++ -c -Wall -Wextra -Werror src/Main.cpp -o bin/ds-start
+g++ -Wall -Wextra -Werror src/Main.cpp -o bin/ds-start
 # To build for Windows, change the `ds-start` to `ds-start.exe`
 # For a UNIX OS (Linux, macOS), just leave it as it is
